5f00c29141318745ae085dd4e27a6c33c7ba3ef8
Mike Parthum <mparthum@gmail.com>
2019-02-01 13:27:20 -0500

    Update about me and fix carousel

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-setInterval(function(){
-    $(".heart").animate({fontSize: "1rem"}, 250);
-    $(".heart").animate({fontSize: ".9rem"}, 250);
+//Heartbeat
+
+setInterval(function() {
+  $(".heart").animate({
+    fontSize: "1rem"
+  }, 250);
+  $(".heart").animate({
+    fontSize: ".9rem"
+  }, 250);
 }, 2000);
+
+$("a").on('click', function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+    var hash = this.hash;
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function() {
+      window.location.hash = hash;
+    });
+  }
+});
